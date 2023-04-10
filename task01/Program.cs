f6d1a66ff684841fcf0bbef46ecafb5e982b9781
ваше_имя <grigorenko-marko@rambler.ru>
2023-04-10 21:03:41 +0300

    task 01 02 03

---
+++ task01/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. ДЗ 4 задача 2
+//Первый вариант, более затратный, через массив
+
+//Console.WriteLine(GetSumFromInt(12345));
+
+int GetSumFromString(string number)
+{
+	int result = 0;
+
+	for (int i = 0; i < number.Length; i++)
+	{
+		result += int.Parse(number[i].ToString());
+	}
+
+	return result;
+}
+
+/*Второй вариант, математический, мез массива
+int GetSumFromInt(int number)
+{
+	int result = 0;
+
+	for (int i = number; i > 0; i /= 10)
+	{
+		result += i % 10;
+	}
+
+	return result;
+}
+*/
---
+++ task02/Program.cs
@@ -0,0 +1,50 @@
+﻿//Напишите программу, которая перевернёт одномерный массив 
+//(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+//[1 2 3 4 5] -> [5 4 3 2 1]
+//[6 7 3 6] -> [6 3 7 6]
+
+int Length = ReadInt("длина массива ");
+int min = ReadInt("минимальное значение ");
+int max = ReadInt("максимальное значение");
+int [] numbers = CreateArray(Length, min, max);
+PrintArray(numbers);
+Reverse(numbers);
+PrintArray(numbers);
+
+
+
+int ReadInt(string arg)
+{
+    System.Console.WriteLine($"Введите {arg}");
+    return int.Parse(Console.ReadLine());
+}
+
+int [] CreateArray(int Length, int min, int max)
+{
+    int [] numbers = new int [Length];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(min, max);
+    }
+    return numbers;
+}
+
+void Reverse(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length/2; i++)
+    {
+        int temp = numbers[i];
+        numbers[i] = numbers[numbers.Length-1-i];
+        numbers[numbers.Length-1-i] = temp;
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < numbers.Length-1; i++)
+    {
+        System.Console.Write(numbers[i] + ";");
+    }
+    System.Console.WriteLine(numbers[numbers.Length-1] + "]");
+}
