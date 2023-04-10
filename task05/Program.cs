1d7b6531bee26303fdd574dc8353e5e08a75f03b
ваше_имя <grigorenko-marko@rambler.ru>
2023-04-10 23:09:28 +0300

    task complite

---
+++ task05/Program.cs
@@ -0,0 +1,28 @@
+﻿//Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи:
+//0 и 1.Если N = 5 -> 0 1 1 2 3
+//Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+
+int ReadInt(string arg)
+{
+   System.Console.Write($"Введите число: ");
+   return int.Parse(Console.ReadLine());
+}
+
+int N = ReadInt("N");
+
+int [] Array(int Length)
+{
+   int [] numbers = new int[Length];
+    numbers[0] = 0;
+    numbers[1] = 1;
+    for (int i = 2; i < numbers.Length; i++)
+    {
+        numbers[i] = numbers[i-1] + numbers[i-2];
+    } 
+    return numbers;
+}
+
+System.Console.WriteLine(string.Join(", ", Array(N) ));
+
---
+++ task06/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+
+int [] CreateArray(int Length, int min, int max)
+{
+int [] numbers = new int [Length];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = new Random().Next(min,max +1);
+}
+return numbers;
+}
+
+void PrintArray(int [] numbers)
+{
+System.Console.Write("[");
+for (int i = 0; i < numbers.Length -1; i++)
+{
+    System.Console.Write(numbers[i] + "; ");
+}
+System.Console.WriteLine(numbers[numbers.Length -1] + "]");
+}
+
+int Num (string arg)
+{
+    System.Console.WriteLine($"Введите {arg} ");
+    return int.Parse(System.Console.ReadLine());
+}
+
+
+int []NewNumbers(int [] numbers)
+{
+int [] NewNumbers = new int [numbers.Length];
+for (int i = 0; i < NewNumbers.Length; i++)
+{
+    NewNumbers[i] = numbers[i];
+}
+return NewNumbers;
+}
+
+
+
+int Length = Num("длину массива");
+int min = Num("меньшее число");
+int max = Num("максимальное число");
+int [] numbers = CreateArray(Length, min, max);
+PrintArray(numbers);
+int [] NewNumber = NewNumbers(numbers);
+PrintArray(NewNumber);
